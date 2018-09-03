2a4720403cee17a9a0f03d8019fe179a5391a2eb
KGCybeX <k4gcybex@gmail.com>
2018-09-03 23:33:17 +0200

    WIP

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,18 @@ import java.util.Random;
 public class Main {
 
     // Chromosome Values
-    public static int[][] tdData,
+    public int[][] tdData,
             vdData,
             edData;
     // Chromosome Weights
-    public static double[][] population;
+    public double[][] population;
 
+    // Population solution size
+    final int populationSize = 80;
     // Number of genes in a chromosome
     final int numberOfGenes = 8;
     // Size of tournament, used in chromosome selection
-    final int tournamentPopSize = 50;
+    final int tournamentPopSize = 10;
 
     // crossover threshold favouring optimal parent
     final double crossoverThreshold = 0.6;
@@ -33,17 +35,25 @@ public class Main {
     // Max generations before stopping
     private int maxGeneration = 1000;
 
-    // Container to store accuracy error (used in determining fitness)
-    int trainingMSE = Integer.MAX_VALUE,
-            testMSE = Integer.MAX_VALUE;
+    /**
+     * Model Index:
+     * 0. Simple Linear
+     * 1. Linear with constant
+     * 2. Exponential with constant
+     */
+    private int modelIndex = 0;
+
+    // Container to store training and testing SSE
+    int[] trainingMSE = new int[populationSize],
+            testMSE = new int[populationSize];
 
 
     private Main() {
         // Evolution Algorithm
         generatePopulation();
 
-        // Determines SSE
-        evaluateFitness(population, 1);
+        // Determines training SSE
+        evaluateFitness();
 
         // Training set
         while (liveOn()) {
@@ -73,10 +83,9 @@ public class Main {
         Random r = new Random(1);
         for (int i = 0; i < offspring.length; i++) {
             for (int i1 = 0; i1 < offspring[i].length; i1++) {
-                offspring[i][i1] *= (r.nextGaussian() > 0.5) ? mutationRate : 1-mutationRate;
+                offspring[i][i1] *= (r.nextGaussian() > 0.5) ? mutationRate : 1 - mutationRate;
             }
         }
-
         return offspring;
     }
 
@@ -91,7 +100,7 @@ public class Main {
             return false;
         }
 
-        if ((1-error) >= requiredAccuracy) {
+        if ((1 - error) >= requiredAccuracy) {
             System.out.println("Solution found at generation " + generation);
             return false;
         }
@@ -118,9 +127,9 @@ public class Main {
     }
 
     // Create offspring from selected parents. This creates offspring by using crossover genes from each parent.
-    private int[] crossover(double[] p1, double[] p2) {
+    private double[] crossover(double[] p1, double[] p2) {
         Random r = new Random();
-        int[] child = new int[p1.length];
+        double[] child = new double[p1.length];
         for (int i = 0; i < p1.length; i++) {
             child[i] = (r.nextGaussian() > crossoverThreshold) ? p1[i] : p2[i];
         }
@@ -133,94 +142,100 @@ public class Main {
      * @param population
      * @return
      */
-    private int tournamentSelect(int[][] population) {
+    private int tournamentSelect(int[] population) {
         Random r = new Random(1);
-        int[][] selected = new int[tournamentPopSize][numberOfGenes];
+        int[] selected = new int[tournamentPopSize];
 
         for (int i = 0; i < tournamentPopSize; i++) {
             Double pos = r.nextGaussian() * population.length;
-            selected[i] = population[pos.intValue()];
+            selected[i] = pos.intValue();
         }
         return getFittest(selected);
     }
 
-    private int[] getFittest(int[][] pop) {
-        for (int i = 0; i < pop.length; i++) {
-            if (getFitness(parent) > getFitness(population[fittestPos]))
-                fittestPos = i;
+    private int getFittest(int[] selected) {
+        int best = 0;
+
+        for (int i = 1; i < selected.length; i++) {
+            int index = selected[i];
+            if (trainingMSE[index] < best) {
+                best = index;
+            }
         }
+
+        return best;
     }
 
-    private double getFitness(int[] gene, int solution) {
-        double error = gene[0] - solution;
-        return Math.abs(error / gene[0]);
-    }
 
-    private int[] evaluateFitness(int[][] population, int[][] data, final int modelNum) {
-        int[] salaries = new int[population.length];
+    /**
+     * Calculates training and testing MSE
+     *
+     * @return
+     */
+    private void evaluateFitness() {
 
-        for (int j = 0; j < population.length; j++) {
-            Double salary = 0.0;
+        // For all solutions in population
+        for (int popIndex = 0; popIndex < populationSize; popIndex++) {
+            Double salaryTrain = Double.MAX_VALUE,
+                    salaryTest = Double.MAX_VALUE;
 
-            for (int i = 0; i < population[j].length; i++) {
-                switch (modelNum) {
-                    case 1: salary += population[j][i] * data[j][i]; break;
-                    case 2: salary += population[j][i] * data[j][i]; break;
-                    case 3:
-                    default: salary += Math.pow(population[j][i] * data[j][i], data[j][i + population.length + 1]); break;
+            // For each model, evaluate the following
+                switch (modelIndex) {
+                    case 0:
+                    case 1:
+                        salaryTrain += population[popIndex][modelIndex] * tdData[popIndex][modelIndex];
+                        salaryTest += population[popIndex][modelIndex] * vdData[popIndex][modelIndex];
+                        break;
+                    case 2:
+                    default:
+                        salaryTrain += Math.pow(population[popIndex][modelIndex] * tdData[popIndex][modelIndex], tdData[popIndex][modelIndex + population.length + 1]);
+                        salaryTest += Math.pow(population[popIndex][modelIndex] * vdData[popIndex][modelIndex], vdData[popIndex][modelIndex + population.length + 1]);
+                        break;
                 }
+
+                if (modelIndex == 1) {
+                    salaryTrain += tdData[popIndex][tdData.length - 1];
+                    salaryTest += vdData[popIndex][vdData.length - 1];
+                } else if (modelIndex == 2) {
+                    salaryTrain += tdData[popIndex][(populationSize / 2) + 1];
+                    salaryTest += vdData[popIndex][(populationSize / 2) + 1];
+                }
+                // TODO fix this
+
+            // Calculate MSE for each model of the current training solution and add to MSE arrays
+            double localerror = 0.0;
+            for (int i = 0; i < tdData.length; i++) {
+                localerror += Math.pow(tdData[popIndex][0] - salaryTrain, 2);
             }
+            trainingMSE[popIndex] = new Double(localerror / tdData.length).intValue();
 
-            if (modelNum == 2) {
-                salary += data[j][data.length - 1];
-            } else if (modelNum == 3) {
-                salary += data[j][(population.length / 2) + 1];
+            // Calculate MSE for each model of the current testing solution and add to MSE arrays
+            localerror = 0.0;
+            for (int i = 0; i < vdData.length; i++) {
+                localerror += Math.pow(vdData[popIndex][0] - salaryTest, 2);
             }
-
-            salaries[j] = salary.intValue();
+            testMSE[popIndex] = new Double(localerror / vdData.length).intValue();
         }
-
-
-        double localerror = 0.0;
-        for (int i = 0; i < tdGene.length; i++) {
-            localerror += getFitness(data[i], predictedSalaries[i]);
-        }
-
-        // determines error in predicting salary
-        error = localerror / tdGene.length;
-
-
-
-        return salaries;
     }
 
     private void generatePopulation() {
-        // Initialize Weights
-        initializeWeights();
+        // Initialize first generation solutions
+        population = new double[populationSize][numberOfGenes];
+        Random random = new Random(1);
+        for (int i = 0; i < populationSize; i++) {
+            for (int i1 = 0; i1 < population[i].length; i1++) {
+                population[i][i1] = random.nextGaussian();
+            }
+
+            trainingMSE[i] = Integer.MAX_VALUE;
+            testMSE[i] = Integer.MAX_VALUE;
+        }
 
         // Read Training and Testing data
         readTrainAndValidationData();
 
         // read Validation Data
         readEvaluationData();
-    }
-
-    private void initializeWeights() {
-        tWeight = new double[tdData.length][numberOfGenes];
-        fillWithRandomWeights(tWeight);
-        vWeight = new double[vdData.length][numberOfGenes];
-        fillWithRandomWeights(vWeight);
-        eWeight = new double[edData.length][numberOfGenes];
-        fillWithRandomWeights(eWeight);
-    }
-
-    private void fillWithRandomWeights(double[][] weightsArray) {
-        Random random = new Random(1);
-        for (int i = 0; i < weightsArray.length; i++) {
-            for (int i1 = 0; i1 < weightsArray[i].length; i1++) {
-                weightsArray[i][i1] = random.nextGaussian();
-            }
-        }
     }
 
     private void readTrainAndValidationData() {
